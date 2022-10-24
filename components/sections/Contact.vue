b310287018e954d5b2ad6abdc65122c2dd16e565
Georgi Georgiev <G.A.Georgiev@student.tudelft.nl>
2022-10-25 01:03:12 +0200

    Add contact section

--- components/sections/Contact.vue
+++ components/sections/Contact.vue
@@ -1,3 +1,24 @@
 <template>
-  <section id="contact"></section>
+  <section
+    id="contact"
+    class="mb-20 flex max-w-[600px] flex-col items-center justify-center"
+  >
+    <h2 class="mt-2 mb-2 flex capitalize">
+      <span class="font-mono text-xs text-primary"> 04. What's next? </span>
+    </h2>
+    <h2 class="mt-2 mb-6 flex capitalize">
+      <span class="text-4xl font-black">Get in touch</span>
+    </h2>
+    <p class="text-center">
+      Although I’m not currently looking for any new opportunities, my inbox is
+      always open. Be it a question or just a spontaneous hello, feel free to
+      shoot me an email and I'll do my best to get back!
+    </p>
+    <base-button
+      link="mailto:gatanasovgeorgiev@gmail.com?subject=Hello&body=Hello%2C%20I%20wanna%20tell%20you%20about%20my%20amazing%20idea..."
+      class="mt-10 capitalize"
+    >
+      Say hello
+    </base-button>
+  </section>
 </template>
